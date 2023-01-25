dd8dd1fb950c85ccb3e79f48443d458de24d50ea
ufenekenn <ufenekenn@gmail.com>
2023-01-25 12:31:54 -0800

    Classes and Objects commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Define a class square."""
+
+
+class square:
+    """Represent a Square."""
+
+    def _init_(self, size):
+        """Initialize a new Square.
+        Args:
+            size (int): the size of the  new square.
+        """
+        self._size = size
